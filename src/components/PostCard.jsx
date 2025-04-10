56d8af050150794cabdadc9cee79c1012e035cba
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2025-04-10 10:17:15 -0500

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 // Use this heart icon to create the like button
 import { Heart } from 'lucide-react';
 
-export function PostCard({ username, imageUrl, caption, likes, onLike }) {
+function PostCard({ username, imageUrl, caption, likes, onLike }) {
   return (
     <div className="post">
      {/* Build your post card here */}
     </div>
   );
 }
+
+export default PostCard
