7c24a9162554f285e410f829739d9a6dfe08b37f
Shukri Mohamed <Shukriiman83@gmail.com>
2025-04-13 10:34:35 +0800

    done week 11 assignment

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,14 +1,54 @@
-import React from 'react';
-
+import React from "react";
+import { useState } from "react";
 // Use this heart icon to create the like button
-import { Heart } from 'lucide-react';
+import { Heart } from "lucide-react";
 
-function PostCard({ username, imageUrl, caption, likes, onLike }) {
+function PostCard({ username, imageUrl, caption, likes, onLike, id }) {
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+
+  const addComment = () => {
+    if (newComment.trim() === "") return;
+    setComments((prev) => [...prev, newComment]);
+    setNewComment("");
+  };
+
   return (
     <div className="post">
-     {/* Build your post card here */}
+      {/* // Build your post card here // */}
+      <h1 className="username">{username}</h1>
+
+      <img src={imageUrl} alt="image" className="post-image" />
+      <p className="post-caption">{caption}</p>
+      <div className="likes">
+        <button onClick={() => onLike(id)}>
+          <Heart
+            size={24}
+            color={likes >= 1 ? "red" : "black"}
+            fill={likes >= 1 ? "red" : "#f5f5f5"}
+          />
+        </button>
+      </div>
+      <p className="likes-count">{likes} Likes</p>
+      <div className="comment">
+        <input
+          type="text"
+          value={newComment}
+          placeholder="add comment"
+          onChange={(e) => setNewComment(e.target.value)}
+        ></input>
+        
+      <button onClick={addComment}>Add</button>
+      </div>
+
+
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default PostCard
+export default PostCard;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import PostCard from "./PostCard"
 // Import the PostCard here
 
-export function PostList({ posts, onLike }) {
+export function PostList({ posts,onLike}) {
   return (
     <div className="post-list">
-     {/* Loop through the posts here and pass to the PostCard */}
+     {/* Loop through the posts here and pass to the PostCard */
+     
+     posts.map((post)=><PostCard
+     id={post.id}
+     key={post.id}
+       username={post.username} 
+       imageUrl={post.imageUrl} 
+       caption={post.caption} 
+       likes={post.likes} 
+       onLike={onLike}
+       comment={post.comment}
+      />)
+     
+     
+     }
     </div>
   );
 }
