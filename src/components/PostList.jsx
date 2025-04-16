24cee19056b6508563748e7e88526deb21c31a78
ak56068 <akmahdi28@gmail.com>
2025-04-16 12:16:07 -0500

    week_11_react_assignment

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PostCard from "./PostCard";
 
 // Import the PostCard here
 
@@ -6,6 +7,23 @@ export function PostList({ posts, onLike }) {
   return (
     <div className="post-list">
      {/* Loop through the posts here and pass to the PostCard */}
+   {posts.map((post, index) => (
+    <PostCard 
+    
+    key={index}
+        username={post.username}
+        imageUrl={post.imageUrl}
+        caption={post.caption}
+        likes={post.likes}
+
+       
+        onLike={() => onLike(index)}
+    
+    />
+
+
+
+   ))}
     </div>
   );
 }
