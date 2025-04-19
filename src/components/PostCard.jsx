64aca6e2916652bbf16106817b0f7175e33ab441
Abdifatah Omar <abdifatahumer@gmail.com>
2025-04-19 01:13:50 -0500

    Completed Week 11 Assignment

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,9 +6,25 @@ import { Heart } from 'lucide-react';
 function PostCard({ username, imageUrl, caption, likes, onLike }) {
   return (
     <div className="post">
-     {/* Build your post card here */}
+      <div className="post-header">
+        
+      <div className='avatar'>{username.charAt(0).toUpperCase()}</div>
+      <div className='username'>{username}</div>
+      </div>
+      <div className='post-image'>
+      <img src={imageUrl} alt="username" className="post-image"/>
+      </div>
+     
+      
+      <div className="post-actions">
+        <button  className="like-button" onClick={onLike}>
+          <Heart/> {likes} likes
+        </button>
+
+      </div>
+      <div className="post-captions"> <strong>{username}</strong>{caption}</div>
     </div>
   );
 }
 
-export default PostCard
+export default PostCard;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 // Import the PostCard here
+import PostCard from './PostCard';
 
 export function PostList({ posts, onLike }) {
   return (
     <div className="post-list">
-     {/* Loop through the posts here and pass to the PostCard */}
+      {posts.map((post,index) => (
+        <PostCard key={index}
+          username={post.username}
+          imageUrl={post.imageUrl}
+          caption={post.caption}
+          likes={post.likes}
+          onLike={() => onLike(index)}
+        />
+      ))}
     </div>
-  );
+  )
 }
+
+export default PostList;
