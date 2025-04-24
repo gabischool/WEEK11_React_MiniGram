907c6d6dea68a560b441a6ecd57a61135ab38602
EngRAAF <salaad79@gmail.com>
2025-04-24 13:39:40 -0700

    finished

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {PostCard} from "./PostCard";
 
 // Import the PostCard here
 
-export function PostList({ posts, onLike }) {
+export function PostList({ posts, onLike}) {
   return (
     <div className="post-list">
-     {/* Loop through the posts here and pass to the PostCard */}
-    </div>
-  );
+     {/* Loop through the posts here and pass to the PostCard */
+     posts.map((post)=> <PostCard
+     id = {post.id}
+     key = {post.id}
+     username = {post.username}
+     imageUrl = {post.imageUrl}
+     caption = {post.caption}
+     Likes ={post.Likes}
+     onLike = {onLike}
+     comment = {post.comment}
+
+    />  )
 }
+    </div>
+  )
+}
+export default PostList;
