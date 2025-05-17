eca84c565f44283aff51048759b3312149fb7e73
Mohazmuhanad <xamadeabdirahman7@gmail.com>
2025-05-18 01:16:03 +0300

    done

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -7,6 +7,16 @@ function PostCard({ username, imageUrl, caption, likes, onLike }) {
   return (
     <div className="post">
      {/* Build your post card here */}
+      <div className="post-header">
+        <h2 className="post-username">{username}</h2>
+      </div>
+      <img src={imageUrl} alt={caption} className="post-image" />
+      <div className="post-caption">
+        <p>{caption}</p>
+      </div>
+      <button onClick={onLike} className="like-button">
+        <Heart size={24} /> {likes}
+      </button>
     </div>
   );
 }
