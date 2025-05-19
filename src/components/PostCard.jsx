6dbcd15d4223556c91e737acc88fcafe1ad1d680
ikran123 <ikrammyyusuf@gmail.com>
2025-05-19 10:01:51 +0300

    done with minigram

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+
 
 // Use this heart icon to create the like button
+import React, { useState } from 'react';
 import { Heart } from 'lucide-react';
 
 function PostCard({ username, imageUrl, caption, likes, onLike }) {
+
+ const [liked, setLiked] = useState(false);
+ const handleClick = () => {
+ onLike();
+ 
+ };
+
   return (
     <div className="post">
      {/* Build your post card here */}
+     <h3>@{username}</h3>
+     <img src={imageUrl} alt="Post" />
+    <p>{caption}</p>
+    <button onClick={handleClick}>
+      <Heart/> Like
+    </button>
+    <span> ({likes}) </span>
+    
+
     </div>
   );
 }
 
-export default PostCard
+export default PostCard;
